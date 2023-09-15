582e49e8febe40a8466a0a63cf5da49f3d6366ae
Gabriel Mathias <gablemathias@icloud.com>
2023-09-15 15:04:45 -0300

    Adding class WC and moving functionalities

--- lib/ccwc.rb
+++ lib/ccwc.rb
@@ -12,6 +12,47 @@
 
 ## Code
 
+class Wc
+  def initialize(file, *args)
+    @file = File.read(file)
+    @args = args || ["-l","-w","-c"]
+  end
+  
+  def read
+    @args.each do |arg|
+      case arg
+      when '-c'
+        bytes
+      when '-l'
+        lines
+      when '-w'
+        words
+      when '-m'
+        characters
+      end
+    end
+  end
+  
+  private
+  attr_reader :file
+  
+  def bytes
+    file.bytesize.to_s
+  end
+  
+  def lines
+    file.each_line.count.to_s
+  end
+  
+  def words
+    file.split.size.to_s
+  end
+  
+  def characters
+    file.each_char.count.to_s
+  end
+end
+
 commands = ARGF.argv
 
 file = commands.pop # last-command file
@@ -22,7 +63,7 @@ commands.each do |arg|
   when '-c'
     result += File.read(file).bytesize.to_s + " "
   when '-l'
-    result += File.readlines(file).size.to_s + " "
+    result += File.read(file).each_line.count.to_s + " "
   when '-w'
     result += File.read(file).split.size.to_s + " "
   when '-m'
